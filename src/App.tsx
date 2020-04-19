973bd15e5470e4cc3ae50a5bda96826f9f35e6d3
adam pauw <adampauw@gmail.com>
2020-04-19 11:48:55 +0200

    revert header to simple way

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,27 @@ import "./App.css";
 import { Navbar } from "./components/navbar/Navbar";
 import styled from "@emotion/styled";
 import Overlay from "./components/header/Overlay";
-import { Header } from "./components/header/Header";
 
 const App: React.FC = () => {
   const Background = styled.div`
-  background-image: url("/img/mountain-fog.jpg");
-  background-size: cover;
-  background-position: center;
-  height: 500px;
-  position: relative;
-`;
+    background-image: url("/img/mountain-fog.jpg");
+    background-size: cover;
+    background-position: center;
+    height: 500px;
+    position: relative;
+  `;
 
   return (
     <div className="App">
-      <Header />
-      <div className="Content" style={{backgroundColor: "#97aeb3", height: "100vh"}}></div>
+      <Background>
+        <Navbar />
+        <Overlay width={100} height={500} position={0} />
+        <Overlay width={20} height={500} position={120} />
+      </Background>
+      <div
+        className="Content"
+        style={{ backgroundColor: "#97aeb3", height: "100vh" }}>
+      </div>
     </div>
   );
 };
