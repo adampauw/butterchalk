447de13f913a69f461218e8681eb7970a47aa0f1
Adam <adampauw@gmail.com>
2020-05-21 18:46:17 +0200

    modify navbar to media icons and install cloudinary

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,27 +1,37 @@
 import React,{FC} from 'react'
-import { NavbarLink } from './NavbarLink';
 import styled from '@emotion/styled';
 
 
 export const Navbar: FC = (props: any) => {
 
   const NavbarContainer = styled.div `
-    width: 10%;
     display: flex;
     flex-direction: column;
     position: absolute;
-    left: 90%;
+    left: 88%;
     padding-top: 1em;
     margin-right: 2%;
-    @media (max-width: 768px) {
-      left: 85%;
+    &:hover a {
+      opacity: 0.5;
+    }
+    a {
+      color: white;
+      margin: 5px;
+      font-size: 25px;
+      &:hover {
+        opacity: 1;
+      }
+    }
+    @media (min-width: 768px) {
+      left: 95%;
     }
   ` 
   return(
     <NavbarContainer>
-      <NavbarLink text="Blog" to="#"/>
-      <NavbarLink text="About" to="#"/>
-      <NavbarLink text="Contact" to="#"/>
+      <a href="https://www.instagram.com/ehpauw/" target="_blank" rel="noopener noreferrer"><i className="fa fa-instagram"></i></a>
+      <a href="https://www.facebook.com/Adam-Pauw-2115750571790087/" target="_blank" rel="noopener noreferrer"><i className="fa fa-facebook-f"></i></a>
+      <a href="https://www.linkedin.com/in/adampauw/" target="_blank" rel="noopener noreferrer"><i className="fa fa-linkedin"></i></a>
+      <a href="https://github.com/adampauw" target="_blank" rel="noopener noreferrer"><i className="fa fa-github"></i></a>
     </NavbarContainer>
   )
 };
